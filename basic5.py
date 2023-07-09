2bd2b2fdbe355fad29edc89ba0000d4c850d6042
wj5811github <wj5811@163.com>
2023-07-09 19:38:26 +0800

    Update basic5.py
    .join()的用法

--- basic5.py
+++ basic5.py
@@ -1,48 +1,10 @@
+# 字符串 .join()的用法
+str_1 = "---"
 
-money = 0
+list_2 = ["a", "b", "c"]
 
-name = input("请输入您的姓名：")
-
-def query(flag):
-    if flag:
-        print("-"*20,"欢迎光临","-"*20)
-    print(f"尊敬的{name},您的帐户现有余额为：{money}元")
-def cunqian():
-    global  money
-    zj = int(input("请输入存钱金额："))
-    money += zj
-    query(False)
-
-def quqian():
-    global  money
-    qq = int(input("请输入取钱金额："))
-    if qq <= money:
-        money -= qq
-        query(False)
-    else:
-        print("余额不足")
-
-def main():
-
-    print("查询请按1")
-    print("存款请按2")
-    print("取款请按3")
-    print("退出请按4")
-
-    keynum = input("请输入你的选择：")
-    if keynum == "1":
-        query(True)
-
-    if keynum == "2":
-        cunqian()
-
-    if keynum == "3":
-        quqian()
-
-    if keynum == "4":
-        print("谢谢光临，下次再见！\n退出程序了")
-
-    return input("请输入你的选择：")
-
-if __name__ == '__main__':
-    main()
+str_2 = "jinanshengmingjisuanji"
+new_list = str_1.join(list_2)
+new_list2 = str_1.join(str_2)
+print(new_list)   # a---b---cnew_list = str_1.join(list_2)
+print(new_list2)   # j---i---n---a---n---s---h---e---n---g---m---i---n---g---j---i---s---u---a---n---j---i
